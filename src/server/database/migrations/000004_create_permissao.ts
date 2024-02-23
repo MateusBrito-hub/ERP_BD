e1db97c4471baa8b0138e517b2847550f4b1a3cd
mauricio <jose.mauricio.a@outlook.com>
2024-02-22 21:31:13 -0300

    Feat: Create Permissao migrate

---
+++ src/server/database/migrations/000004_create_permissao.ts
@@ -0,0 +1,23 @@
+import type { Knex } from 'knex';
+import { ETableNames } from '../ETableNames';
+
+
+export async function up(knex: Knex){
+	return knex
+		.schema
+		.createTable(ETableNames.permissao, table => {
+			table.bigIncrements('id').primary().index();
+			table.string('nome').notNullable().index();
+			table.boolean('habilitar_padrao').notNullable();
+			table.string('modulo_grupo').notNullable();
+			table.timestamps(true,true);
+
+			table.comment('Tabela usada para armazenar informação de permissoes');
+		});
+}
+
+
+export async function down(knex: Knex){
+	return knex.schema.dropTable(ETableNames.permissao);
+}
+
---
+++ src/server/database/models/Permissao.ts
@@ -0,0 +1,6 @@
+export interface IPermissao {
+    id: number,
+    nome: string,
+    habilitar_padrao: boolean,
+    modulo_grupo: string,
+}
