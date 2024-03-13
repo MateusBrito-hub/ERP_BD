6674f838738376cdc58105f233648260e6c609a3
Brito <mateusvsbrito6@gmail.com>
2024-03-13 13:38:38 -0300

    Feat: Add controllers in Index and create routers

--- src/server/controllers/empresas/index.ts
+++ src/server/controllers/empresas/index.ts
@@ -1,7 +1,15 @@
-import * as create from './Create';
+import * as create  from './Create';
+import * as getAll  from './GetAll';
+import * as getById from './GetById';
+import * as UpdateById from './UpdateById';
+import * as deleteById from './DeleteById';
 
 
 export const EmpresaController = {
-	...create
+	...create,
+    ...getAll,
+    ...getById,
+    ...UpdateById,
+    ...deleteById
 };
 
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,8 +4,20 @@ import { EmpresaController } from '../controllers';
 
 const router = Router();
 
+router.get('/empresa', 
+    EmpresaController.getAllValidation,
+    EmpresaController.getAll);
 router.post('/empresa', 
-    EmpresaController.createValidation, 
+    EmpresaController.createValidation,
     EmpresaController.create);
+router.get('/empresa/:id', 
+    EmpresaController.getByIdValidation,
+    EmpresaController.getById);
+router.delete('/empresa/:id', 
+    EmpresaController.deleteByIdValidation,
+    EmpresaController.deteleById);
+router.put('/empresa/:id', 
+    EmpresaController.updateByIdValidation,
+    EmpresaController.updateById);
 
 export {router};
